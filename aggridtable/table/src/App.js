2f733ca36ca5ea2c150ac7c3bd8c35782b5a50dd
Prabhu599 <prabhuadakai@gmail.com>
2022-08-14 12:55:22 +0530

    table

--- aggridtable/table/src/App.js
+++ aggridtable/table/src/App.js
@@ -1,11 +1,13 @@
   import React from "react";
+  import { BasicTable } from "./components/datatable";
 
 
 export const App = () => {
   return (
+    
     <div >
-      Hello!
+      <BasicTable/>
     </div>
-  );
+  )
 }
 
---
+++ aggridtable/table/src/data.js
@@ -0,0 +1,37 @@
+export const tableData =()=>[
+    {
+        id:1,
+        name:"Kiran kumar",
+        address:"Pune",
+        date:"11/09/2015",
+        order:121,
+    },
+    {
+        id:2,
+        name:"Arun kumar",
+        address:"Delhi",
+        date:"11/09/2016",
+        order:122,
+    },
+    {
+        id:3,
+        name:"Kiran kumar",
+        address:"bangalore",
+        date:"11/09/2017",
+        order:123,
+    },
+    {
+        id:4,
+        name:"kumar",
+        address:"Mysore",
+        date:"11/09/2018",
+        order:124,
+    },
+    {
+        id:5,
+        name:"Pranshant Kumar",
+        address:"Mustari",
+        date:"11/09/2019",
+        order:125,
+    },
+]
